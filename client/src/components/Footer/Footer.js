5d709be20c5995381755264be04773cb31c4a86e
gbielanova <gbielanova@gmail.com>
2021-05-30 21:56:24 +0200

    Show player for all

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './Footer.css';
 import Player from '../Player/Player';
-import { useDataLayerValue } from '../../DataLayer';
 
 
 function Footer() {
-    const [{ accessToken, premium }] = useDataLayerValue();
-
     return (
         <footer className="footer">
             {
-                (premium) ?
-                    <Player accessToken={accessToken} /> :
-                    <p>Player available only for premium users </p>
+                <Player />
+                // (premium) ?
+                //     <Player accessToken={accessToken} /> :
+                //     <p>Player available only for premium users </p>
             }
         </footer>
     );
