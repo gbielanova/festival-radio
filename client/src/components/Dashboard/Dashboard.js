b1871fa40bd17621382162865ccc51d0267a76a2
gbielanova <gbielanova@gmail.com>
2021-05-30 09:46:46 +0200

    Added logout

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import Sidebar from '../Sidebar/Sidebar';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import Gallery from '../Gallery/Gallery';
+import Header from '../Header/Header';
 import SpotifyWebApi from 'spotify-web-api-node';
 import './Dashboard.css';
 import { useDataLayerValue } from '../../DataLayer';
@@ -17,6 +18,21 @@ const spotifyApi = new SpotifyWebApi({
 
 function Dashboard() {
     const [{ accessToken, playingPlaylist }, dispatch] = useDataLayerValue();
+
+    function handleClick() {
+        dispatch({
+            type: "SET_ACCESS_TOKEN",
+            user: null,
+        });
+        dispatch({
+            type: "SET_REFRESH_TOKEN",
+            user: null,
+        });
+        dispatch({
+            type: "SET_EXPIRES_IN",
+            user: null,
+        });
+    }
 
     useEffect(() => {
         if (!accessToken) return;
@@ -37,24 +53,26 @@ function Dashboard() {
             }
         });
 
-        ['5VVoHEuqZBs6kATsF7nLTS', '6ihAiSvGzfRkBZFf8F2vL9', '3iQL47pHJ9YeAbSeXv4Zbp', 
-        '5unwHGGK3FdtuCuREik2xv', '13maJFCugAJicrteZ095TD', '5R8E4lPChZ7l6dNrihfFQS', 
-        '3ivG9ZxjtnERiJWQtyJYFC', '0t1rNc9h9MnPVJcotS0Bng', '4f9CawPddxzOUSJUKj68TQ', 
-        '6bHiAAxR0CLBOozXSyGfrz', '3KxnnQenLSYEMNuTh4gsxm'].map(id => (
-            spotifyApi.getPlaylist(id).then(res => {
-                dispatch({
-                    type: "SET_PLAYLISTS",
-                    playlist: {
-                        description: res.body.description,
-                        name: res.body.name,
-                        id: res.body.id,
-                        href: res.body.href,
-                        tracks: res.body.tracks,
-                        image: res.body.images[0].url,
-                    },
-                });
-            })
-        ));
+        ['5VVoHEuqZBs6kATsF7nLTS', '6ihAiSvGzfRkBZFf8F2vL9', '3iQL47pHJ9YeAbSeXv4Zbp',
+            '5unwHGGK3FdtuCuREik2xv', '13maJFCugAJicrteZ095TD', '5R8E4lPChZ7l6dNrihfFQS',
+            '3ivG9ZxjtnERiJWQtyJYFC', '0t1rNc9h9MnPVJcotS0Bng', '4f9CawPddxzOUSJUKj68TQ',
+            '6bHiAAxR0CLBOozXSyGfrz', '3KxnnQenLSYEMNuTh4gsxm'].map(id => (
+                spotifyApi.getPlaylist(id).then(res => {
+                    dispatch({
+                        type: "SET_PLAYLISTS",
+                        playlist: {
+                            description: res.body.description,
+                            name: res.body.name,
+                            id: res.body.id,
+                            href: res.body.href,
+                            tracks: res.body.tracks,
+                            image: res.body.images[0].url,
+                        },
+                    });
+                })
+            ));
+
+
 
     }, [accessToken, dispatch])
 
@@ -62,8 +80,10 @@ function Dashboard() {
         <div>
             <section className="dashboard">
                 <Sidebar />
-                {playingPlaylist ? <Main /> : <Gallery />}
-
+                <div className='main__wrapper'>
+                    <Header handleClick={handleClick} />
+                    {playingPlaylist ? <Main /> : <Gallery />}
+                </div>
             </section>
             <Footer />
         </div >
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+import { useDataLayerValue } from '../../DataLayer';
+import { Avatar } from "@material-ui/core";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+
+function Header({ handleClick }) {
+    const [{ user }] = useDataLayerValue();
+
+    return (
+        <div className='header' >
+            <div className="header__wrapper">
+                <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+                <p className='header__text'>{user?.display_name}</p>
+                <ExitToAppIcon className='header__logout' onClick={handleClick} />
+            </div>
+        </div>
+    );
+}
+
+export default Header;
