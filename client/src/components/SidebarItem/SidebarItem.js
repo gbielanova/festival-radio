cd722004964c79ac2838aedf88ff228a6a3ea9a7
gbielanova <gbielanova@gmail.com>
2021-05-30 10:24:11 +0200

    Added play icon to playlist

--- client/src/components/SidebarItem/SidebarItem.js
+++ client/src/components/SidebarItem/SidebarItem.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './SidebarItem.css';
+import { useDataLayerValue } from '../../DataLayer';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
 
 function SidebarItem({ title, playlist, Icon, choosePlaylist }) {
+    const [{ playingPlaylist }] = useDataLayerValue();
 
     function handlePlaylistClick() {
         choosePlaylist && choosePlaylist(playlist);
     }
 
+    let activePlaylist = playingPlaylist === playlist;
+
     return (
-        <div className="sidebarItem" onClick={handlePlaylistClick.bind(this)}>
+        <div className={`sidebarItem ${activePlaylist && 'sideItem-active'}`} onClick={handlePlaylistClick.bind(this)}>
+            {activePlaylist && <PlayArrowIcon />}
             {Icon && <Icon className='sidebarItem__icon' />}
             {Icon ? <h4>{title}</h4> : <p>{playlist.name}</p>}
         </div>
