9ecec5e39cf39b5eb2dfc23afeef09397297bc0f
gbielanova <gbielanova@gmail.com>
2021-06-05 11:35:56 +0200

    Trying fix module not found error

--- api/festival_radio_project/settings.py
+++ api/festival_radio_project/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "festival_radio_api",
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
