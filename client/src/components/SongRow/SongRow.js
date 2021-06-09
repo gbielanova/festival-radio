7f66fabaa698298a506714f25bf985548906964c
gbielanova <gbielanova@gmail.com>
2021-06-09 16:13:18 +0200

    Show play button only for premium

--- client/src/components/SongRow/SongRow.js
+++ client/src/components/SongRow/SongRow.js
@@ -5,13 +5,13 @@ import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 
 function SongRow({ track, playSong }) {
-    const [{ playingPlaylist, playingTrack }] = useDataLayerValue();
+    const [{ playingPlaylist, playingTrack, premium }] = useDataLayerValue();
 
     if (!track) return (<></>);
 
     return (
         <div className={`songRow ${(playingTrack === track) ? 'songRow-active' : ''}`} onClick={() => playSong(track)}>
-            {(playingTrack === track) && <PlayArrowIcon className='songRow__icon' />}
+            {((playingTrack === track) && premium) && <PlayArrowIcon className='songRow__icon' />}
             <img className='songRow__img' src={track.album?.images[0]?.url || playingPlaylist?.image} alt="" />
             <div className='songRow__info'>
                 <h3 className='songRow__name'>{track.name}</h3>
